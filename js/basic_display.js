5a94fb8b99a4d95bc48c699e695c8b94ddbb8297
Iru <iru@Irus-Mac.local>
2014-08-20 13:14:03 -0700

    new categ UI

---
+++ js/basic_display.js
@@ -0,0 +1,8 @@
+function level_progress(level_element_id, progress){
+	var level_element = document.getElementById(level_element_id);
+	var progress = parseInt(progress);
+	var whitewidth = 100 - progress;
+	var imageText = (("linear-gradient(to bottom, #FFFFFF ").concat(whitewidth.toString())).concat("%,#ffc17f 0%)")
+	level_display.style.backgroundImage = imageText;
+}
+
