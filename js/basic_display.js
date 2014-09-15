8a5a6e28468b523207cd8192de0779c615018974
Shawn <s@yoursapp.cc>
2014-09-15 11:37:47 -0700

    Fix small bugs in red badge

--- js/basic_display.js
+++ js/basic_display.js
@@ -9,7 +9,7 @@
   
 $(document).ready(function() {
 
-
+  console.log($("#chat_dot_display"));
   if( $("#chat_dot_display").html() == "0") {
     $("#chat_dot_display").css("opacity", 0);
   }
@@ -17,7 +17,7 @@ $(document).ready(function() {
   var ses = new EventSource('/status_notif');
   ses.onmessage = function(e) {
     console.log("status: " + e.data);
-    if( $("#chat_dot_display").html() != "0") {
+    if( e.data != "0") {
       $("#chat_dot_display").css("opacity", 1).html(e.data);
     } else {
       $("#chat_dot_display").css("opacity", 0);
